d814984b07c6581ffd3d42ac242b60bcb65e4b07
AzzouziNouamane <nouamane.azzouzi@etu.unice.fr>
2019-08-03 23:43:54 +0200

    Program works with voice recognition.

--- screenshotTester.py
+++ screenshotTester.py
@@ -1,25 +1,38 @@
 from pynput.keyboard import Key, KeyCode, Listener
 import pyscreenshot as ImageGrab
 import datetime
+import speechRecognitionTester as speechRecognitionTester
+import time
+import pyautogui
 
 # Your functions
 
 def take_screenshot():
-    if __name__ == '__main__':
-        print(ImageGrab.grab())
-    # grab fullscreen
-        im = ImageGrab.grab()
-
-    # save image file
-        im.save('screenshot.png')
-
-    # show image in a window
-        im.show()
+    print('Executing...')
+    #if __name__ == '__main__':
+    x = 1
+    while x < 4 :
+        pyautogui.screenshot('image'+str(x)+'.png')
+        x += 1
+        time.sleep(2)
 #-#
 
 def test_hotkey():
     with open("D:\logtext.txt", 'a') as File:
         File.write(str(datetime.datetime.now()) + '\n')
 
-take_screenshot()
-test_hotkey()
+while (True):
+    speech = speechRecognitionTester.speech_recognition()
+    if speech is None:
+        continue
+    print (speech) 
+    if('1' in speech):
+        print('Gotta take a screen nigga')
+        take_screenshot()
+        #time.sleep(10)
+        
+    if('stop' in speech):
+        break
+
+
+exit()
